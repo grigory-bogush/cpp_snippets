f43e0edb25fc8c2891a6febd99eae26b56c44b9a
Grigory Bogush <grigory.bogush.dev@gmail.com>
2023-01-29 01:27:26 +0400

    render rotated triangle using glm

--- gl_render_triangle.cpp
+++ gl_render_triangle.cpp
@@ -1,11 +1,29 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+glm::vec3 triangle_vertecies[3] = {
+    {-0.5, -0.5, 1.0f},
+    {0.5, -0.5, 1.0f},
+    {0.0, 0.5, 1.0f},
+};
+float angle_rad = 1.0f;// 1/PI of a circle
+glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), angle_rad, glm::vec3(0.0f, 0.0f, 1.0f));
 
 void renderTriangle() {
+
+    // Transform the triangle's vertices
+    for (int i = 0; i < 3; i++) {
+        glm::vec4 vertex = glm::vec4(triangle_vertecies[i], 1.0f);
+        vertex = rotationMatrix * vertex;
+        triangle_vertecies[i] = glm::vec3(vertex);
+    }
+
+
     glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-        glVertex2f(0.0, 0.5);
+        for (auto vertex : triangle_vertecies) {
+            glVertex3f(vertex[0], vertex[1], vertex[2]);
+        }
     glEnd();
 }
 
